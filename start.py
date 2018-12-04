4f4b103bb4c4522f8a436bd0a5746671f543c5c9
AnchoR29 <cruzmarnoel@gmail.com>
2018-12-04 17:02:51 +0800

    Added Flask-Migrate

--- start.py
+++ start.py
@@ -1,4 +1,12 @@
-from dataweeb import app
+from dataweeb import app,db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=False)
+    manager.run()
